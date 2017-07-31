17801504fd89e0643695ce39ec02005cb97c9f99
Louis-Saglio <louis.saglio@ynov.com>
2017-07-31 03:18:19 +0200

    Début de l'implémentation

--- sample/player.py
+++ sample/player.py
@@ -1,4 +1,8 @@
-from armee import Armee
+import ia
+
+
+class PlayerError(BaseException):
+    pass
 
 
 class Player:
@@ -6,4 +10,22 @@ class Player:
     Contient les actions possible pour un joueur
     Ne doit pas contenir de prise de décision (AI)
     """
-    pass
+    def __init__(self):
+        self.unites_disponibles = 0
+        self.ai = ia.base_ai.AI(self)
+
+    def placer_unite(self):
+        if self.unites_disponibles < 1:
+            raise PlayerError
+        self.unites_disponibles -= 0
+        self.ai.placer_unite().nbr_unites += 1
+
+    def attaquer_territoire(self, origine, territoire):
+        """
+        :type origine: territoire.Territoire
+        :type territoire territoire.Territoire
+        """
+        armee_attaquant = origine.former_armee(self.ai.choisir_nbr_attaquants())
+        armee_defenseur = territoire.former_armee(territoire.nbr_unites)
+        armee_attaquant.initialise_attaque(armee_defenseur)
+        armee_attaquant.attaquer()
