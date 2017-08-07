d1b1fac8c70253b8e71e98ffff9bb532300ca6aa
Louis-Saglio <louis.saglio@ynov.com>
2017-08-07 23:02:37 +0200

    [MOD] plateau _> manager [DEL] find_continent_by_name()

--- sample/territoire.py
+++ sample/territoire.py
@@ -1,6 +1,6 @@
 import sample.armee
-import sample.plateau
 import sample.continent
+import engine.game_manager as gm
 
 
 class TerritoireError(BaseException):
@@ -9,12 +9,12 @@ class TerritoireError(BaseException):
 
 class Territoire:
 
-    def __init__(self, nom, plateau, continent, icone):
+    def __init__(self, nom, manager, continent, icone):
         # TODO: changer les tuples en list
-        self.plateau = plateau
+        self.manager = manager
         self.nom = nom
         self.icone = icone
-        self.continent = self.find_continent_by_name(continent)
+        self.continent = self.manager.find_continent_by_name(continent)
         self.continent.territoires += self,
         self.proprietaire = None  # :type sample.player.Player
         self.nbr_unites = 0
@@ -25,15 +25,6 @@ class Territoire:
     def check_attr(self):
         if not isinstance(self.continent, sample.continent.Continent):
             raise sample.plateau.PlateauError(f"{self.continent}, {type(self.continent)}, {sample.continent.Continent}")
-
-    def find_continent_by_name(self, name):
-        if isinstance(name, sample.continent.Continent):
-            return name
-        for cont in self.plateau.continent:
-            if cont.nom == name:
-                return cont
-        raise sample.plateau.PlateauError(f"Aucun continent '{name}' trouvé dans le plateau {self.plateau}\n"
-                                          f"Nom(s) disponibles : {[plt.nom for plt in self.plateau.continent]}")
 
     def link_with(self, other):
         """
