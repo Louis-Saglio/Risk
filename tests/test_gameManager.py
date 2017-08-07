90c85b8e1e75e17b006694b78970b8d0c801b789
Louis-Saglio <louis.saglio@ynov.com>
2017-08-07 23:04:38 +0200

    [DEL] test_create_elements()

--- tests/test_gameManager.py
+++ tests/test_gameManager.py
@@ -3,12 +3,13 @@ from game_manager import GameManager
 
 
 class TestGameManager(TestCase):
+    pass
 
-    def test_create_elements(self):
-        # TODO créer un jeu de données de test dans data_for_tests
-        from data.data_content import PLATEAU
-        test = GameManager()
-        plateau = test.create_elements("plateau", False, "new")
-        assert plateau is PLATEAU
-        test.create_elements("continent", True, plateau)
-        test.create_elements("territoire", True, plateau)
+    # def test_create_elements(self):
+    #     # TODO créer un jeu de données de test dans data_for_tests
+    #     from data.data_content import PLATEAU
+    #     test = GameManager()
+    #     plateau = test.create_elements("plateau", False, "new")
+    #     assert plateau is PLATEAU
+    #     test.create_elements("continent", True, plateau)
+    #     test.create_elements("territoire", True, plateau)
