e61b011066cc8fcc2b79fb9839848daaa71e455e
Louis-Saglio <louis.saglio@ynov.com>
2017-08-07 23:00:16 +0200

    [MOD] plateau -> manager; gestion de owner [ADD] change_owner(); manage_owner()

--- sample/continent.py
+++ sample/continent.py
@@ -1,11 +1,13 @@
 class Continent:
 
-    def __init__(self, nom, renforts: int, plateau, territoires: tuple=()):
+    def __init__(self, nom, renforts: int, manager, territoires: tuple=()):
         self.nom = nom
         self.territoires = territoires  # type: tuple[sample.territoire.Territoire]
         self.renforts = renforts
-        self.plateau = plateau
-        self.plateau.continent += self,
+        self.manager = manager
+        self.manager.continents.append(self)
+        # noinspection PyNoneFunctionAssignment
+        self.owner = self.get_master()
 
     def __str__(self):
         # return f"{self.nom} {id(self.plateau)}"
@@ -20,7 +22,21 @@ class Continent:
         Sinon renvoi None
         """
         # TODO test
+        if not self.territoires:
+            return None
         for territoire in self.territoires[1::]:
             if territoire.proprietaire is not self.territoires[0].proprietaire:
                 return None
-        return self.territoires[0].proprietaire
+        if self.territoires:
+            return self.territoires[0].proprietaire
+
+    def change_owner(self, new_owner):
+        if self.owner is not None:
+            self.owner.continents.remove(self)
+            self.owner = new_owner
+            self.owner.continents.append(self)
+        else:
+            self.owner = new_owner
+
+    def manage_owner(self):
+        self.change_owner(self.get_master())
