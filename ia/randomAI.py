7804a7318b0e254af88570aef1fedde32a13d39a
Louis-Saglio <louis.saglio@ynov.com>
2017-08-30 18:32:59 +0200

    [MOD] fix import issue [ADD] choose_troop_transfert()

--- ia/randomAI.py
+++ ia/randomAI.py
@@ -1,9 +1,13 @@
 from random import choice, randint
 import sample.territoire as trt
-import base_ai
+import ia.base_ai
 
 
-class RandomAi(base_ai.AI):
+class RandomAi(ia.base_ai.AI):
+
+    def choose_troop_transfer(self) -> dict:
+        territoires = [territoire for territoire in self.player.territoires if territoire.nbr_unites > 1]
+        return {"from_trt": choice(territoires), "to_trt": self.player.territoires[0].voisins[0], "nbr_units": 1}
 
     def choose_attacker_number(self, attacker: trt.Territoire, defender: trt.Territoire) -> int:
         pass
