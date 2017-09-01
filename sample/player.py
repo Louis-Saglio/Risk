058cd35c72c330e57ea4c915578ce0d4f9a38548
Louis-Saglio <louis.saglio@ynov.com>
2017-09-01 08:53:54 +0200

    [ADD] Mission, color [MOD] Improve gestion ia

--- sample/player.py
+++ sample/player.py
@@ -1,10 +1,10 @@
 from typing import Type
 import ia.base_ai
-import data
 import sample.territoire as trt
 import sample.armee as arm
 import random
 import engine.game_manager as gm
+import missions.base_mission
 from armee import PERDANT
 
 
@@ -18,13 +18,22 @@ class Player:
     Ne doit pas contenir de prise de décision (AI)
     """
 
-    def __init__(self, manager: gm.GameManager, ai: Type[ia.base_ai.AI]=None):
+    def __init__(self, manager: gm.GameManager, ai: Type[ia.base_ai.AI]=None, color: str=None, mission=None):
+        # todo: name unique, change_manager(), add/remove_territoire/continent/card, classe mere
         self.manager = manager
+        self.manager.players.append(self)
+        self.color = color
         self.territoires = []
         self.continents = []
         self.cards = []
-        if ai is not None:
+        if isinstance(ai, type) and issubclass(ai, ia.base_ai.AI):
             self.ai = ai(self)
+        else:
+            self.ai = ai
+        if isinstance(mission, type) and issubclass(mission, missions.base_mission.BaseMission):
+            self.mission = mission(self)
+        else:
+            self.mission = mission
 
     def __get_continent_reinforcements(self) -> int:
         return sum([continent.renforts for continent in self.continents])
@@ -34,7 +43,7 @@ class Player:
         return number if number >= 3 else 3
 
     def __get_card_set_reinforcements(self) -> int:
-        return data.data_content.CARD_SETS.get(self.ai.choose_card_set()) or 0
+        return self.manager.card_sets.get(self.ai.choose_card_set()) or 0
 
     def _get_reinforcements_number(self) -> int:
         reinforcements = (
@@ -90,3 +99,4 @@ class Player:
         self.manage_reinforcements()
         self.manage_attacks()
         self.manage_troop_transfers()
+        self.mission.is_accomplished()
